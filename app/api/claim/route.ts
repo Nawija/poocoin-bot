54b02d36b882c97c878fd758022975511bf0cd59
Seovileoo <96625039+Nawija@users.noreply.github.com>
2025-09-12 17:31:32 +0200

    dfg

--- app/api/claim/route.ts
+++ app/api/claim/route.ts
@@ -4,8 +4,8 @@ import { sql } from "@vercel/postgres";
 import { initDb } from "@/lib/db";
 
 export async function GET() {
-  await initDb();
-  const result = await sql`
+    await initDb();
+    const result = await sql`
     SELECT 
       id, 
       name, 
@@ -16,51 +16,49 @@ export async function GET() {
     FROM claims
     ORDER BY createdat DESC
   `;
-  return NextResponse.json(result.rows);
+    return NextResponse.json(result.rows);
 }
 
 export async function POST(req: Request) {
-  await initDb();
-  const { name, email, description } = await req.json();
+    await initDb();
+    const { name, email, description } = await req.json();
 
-  if (!name || !email || !description) {
-    return NextResponse.json(
-      { error: "Wszystkie pola są wymagane." },
-      { status: 400 }
-    );
-  }
+    if (!name || !email || !description) {
+        return NextResponse.json(
+            { error: "Wszystkie pola są wymagane." },
+            { status: 400 }
+        );
+    }
 
-  // ustawiamy createdat (teraz) i due_date (za 14 dni)
-  const createdAt = new Date();
-  const dueDate = new Date();
-  dueDate.setDate(dueDate.getDate() + 14);
+    // ustawiamy createdat (teraz) i due_date (za 14 dni)
+    const createdAt = new Date();
+    const dueDate = new Date();
+    dueDate.setDate(dueDate.getDate() + 14);
 
-  await sql`
+    await sql`
     INSERT INTO claims (name, email, description, createdat, due_date)
     VALUES (${name}, ${email}, ${description}, ${createdAt.toISOString()}, ${dueDate.toISOString()});
   `;
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST,
-    port: Number(process.env.SMTP_PORT),
-    secure: true,
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
-    },
-  });
+    const transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
+        },
+    });
 
-  await transporter.sendMail({
-    from: `"System reklamacji" <${process.env.SMTP_USER}>`,
-    to: "konradwiel@interia.pl",
-    subject: "Nowa reklamacja",
-    text: `Nowa reklamacja od: ${name} (${email})
+    await transporter.sendMail({
+        from: `"System reklamacji" <${process.env.EMAIL_USER}>`,
+        to: "reklamacje.siedlce@mebloo.pl",
+        subject: "Nowa reklamacja",
+        text: `Nowa reklamacja od: ${name} (${email})
     
 Opis: ${description}
 
 Dodana: ${createdAt.toLocaleDateString()}
 Kończy się: ${dueDate.toLocaleDateString()}`,
-  });
+    });
 
-  return NextResponse.json({ ok: true });
+    return NextResponse.json({ ok: true });
 }
